b455936ddf2ea378ba7214449b28001e38ce2075
koojiyoung <wldud3628@naver.com>
2022-03-06 20:38:19 +0900

    공통 모듈 분리 signin page 추가

--- src/js/index.js
+++ src/js/index.js
@@ -1,35 +1,5 @@
-// header
-const navbar = document.querySelector("header");
-const navbarHeight = navbar.getBoundingClientRect().height;
-window.addEventListener("scroll", function () {
-  if (window.pageYOffset > navbarHeight) {
-    navbar.classList.add("header-fix");
-  } else {
-    navbar.classList.remove("header-fix");
-  }
-});
-
-// search
-const searchEl = document.querySelector(".search");
-const searchInputEl = searchEl.querySelector("input");
-
-searchEl.addEventListener("click", function () {
-  searchInputEl.focus();
-});
-
-searchInputEl.addEventListener("focus", function () {
-  searchEl.classList.add("focused");
-  searchInputEl.setAttribute("placeholder", "통합검색");
-});
-
-searchInputEl.addEventListener("blur", function () {
-  searchEl.classList.remove("focused");
-  searchInputEl.setAttribute("placeholder", "");
-});
-
 // badges scroll hidden
 const badgeEl = document.querySelector("header .badges");
-const toTopEl = document.querySelector("#to-top");
 window.addEventListener(
   "scroll",
   _.throttle(function () {
@@ -107,22 +77,6 @@ const communitySwiper = new Swiper(".community .swiper-container", {
   // centeredSliders: true,
   autoplay: true,
   loop: true,
-});
-
-// sitempa toggle
-const sitemapEl = document.querySelector(".sitemap .inner");
-const toggleSitemapEl = document.querySelector(".sitemap .toggle-sitemap");
-const toggleExpand = document.querySelector(".expand");
-let isHideSitemap = false;
-toggleSitemapEl.addEventListener("click", function () {
-  isHideSitemap = !isHideSitemap;
-  if (isHideSitemap) {
-    sitemapEl.classList.add("hide");
-    toggleSitemapEl.classList.add("expand");
-  } else {
-    sitemapEl.classList.remove("hide");
-    toggleSitemapEl.classList.remove("expand");
-  }
 });
 
 // scroll animation
